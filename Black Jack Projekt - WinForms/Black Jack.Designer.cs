65946d4078944cb3eb24d1c99a647edf28715b3f
Pingvu <nhlpool27@gmail.com>
2024-01-30 14:55:51 +0100

    Add project files.

---
+++ Black Jack Projekt - WinForms/Black Jack.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ReglerKnapp = new System.Windows.Forms.Button();
+            this.ReglerText = new System.Windows.Forms.Label();
+            this.Start = new System.Windows.Forms.Button();
+            this.Sparfiler = new System.Windows.Forms.Button();
+            this.sparListaBakgrund = new System.Windows.Forms.PictureBox();
+            this.Save1 = new System.Windows.Forms.Button();
+            this.Save2 = new System.Windows.Forms.Button();
+            this.Save3 = new System.Windows.Forms.Button();
+            this.Save4 = new System.Windows.Forms.Button();
+            this.Saldo = new System.Windows.Forms.Label();
+            this.SparaKnapp = new System.Windows.Forms.Button();
+            this.LaddaKnapp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sparListaBakgrund)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ReglerKnapp
+            // 
+            this.ReglerKnapp.BackColor = System.Drawing.Color.Transparent;
+            this.ReglerKnapp.Font = new System.Drawing.Font("Segoe Print", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ReglerKnapp.Location = new System.Drawing.Point(687, 190);
+            this.ReglerKnapp.Name = "ReglerKnapp";
+            this.ReglerKnapp.Size = new System.Drawing.Size(285, 111);
+            this.ReglerKnapp.TabIndex = 0;
+            this.ReglerKnapp.Text = "Regler";
+            this.ReglerKnapp.UseVisualStyleBackColor = false;
+            this.ReglerKnapp.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ReglerText
+            // 
+            this.ReglerText.AutoSize = true;
+            this.ReglerText.Location = new System.Drawing.Point(1032, 209);
+            this.ReglerText.Name = "ReglerText";
+            this.ReglerText.Size = new System.Drawing.Size(347, 78);
+            this.ReglerText.TabIndex = 1;
+            this.ReglerText.Text = resources.GetString("ReglerText.Text");
+            this.ReglerText.Visible = false;
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.Transparent;
+            this.Start.Font = new System.Drawing.Font("Segoe Print", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.Location = new System.Drawing.Point(687, 43);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(285, 111);
+            this.Start.TabIndex = 2;
+            this.Start.Text = "Starta";
+            this.Start.UseVisualStyleBackColor = false;
+            // 
+            // Sparfiler
+            // 
+            this.Sparfiler.BackColor = System.Drawing.Color.Transparent;
+            this.Sparfiler.Font = new System.Drawing.Font("Segoe Print", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sparfiler.Location = new System.Drawing.Point(38, 43);
+            this.Sparfiler.Name = "Sparfiler";
+            this.Sparfiler.Size = new System.Drawing.Size(285, 111);
+            this.Sparfiler.TabIndex = 3;
+            this.Sparfiler.Text = "Sparfiler";
+            this.Sparfiler.UseVisualStyleBackColor = false;
+            this.Sparfiler.Click += new System.EventHandler(this.Sparfiler_Click);
+            // 
+            // sparListaBakgrund
+            // 
+            this.sparListaBakgrund.BackColor = System.Drawing.Color.Transparent;
+            this.sparListaBakgrund.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.gray;
+            this.sparListaBakgrund.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.sparListaBakgrund.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.sparListaBakgrund.Image = global::WindowsFormsApp1.Properties.Resources.gray;
+            this.sparListaBakgrund.Location = new System.Drawing.Point(38, 190);
+            this.sparListaBakgrund.Name = "sparListaBakgrund";
+            this.sparListaBakgrund.Size = new System.Drawing.Size(285, 314);
+            this.sparListaBakgrund.TabIndex = 4;
+            this.sparListaBakgrund.TabStop = false;
+            this.sparListaBakgrund.Visible = false;
+            // 
+            // Save1
+            // 
+            this.Save1.Location = new System.Drawing.Point(52, 207);
+            this.Save1.Name = "Save1";
+            this.Save1.Size = new System.Drawing.Size(259, 67);
+            this.Save1.TabIndex = 5;
+            this.Save1.Text = "Save 1";
+            this.Save1.UseVisualStyleBackColor = true;
+            this.Save1.Visible = false;
+            this.Save1.Click += new System.EventHandler(this.Save1_Click);
+            // 
+            // Save2
+            // 
+            this.Save2.Location = new System.Drawing.Point(52, 280);
+            this.Save2.Name = "Save2";
+            this.Save2.Size = new System.Drawing.Size(259, 67);
+            this.Save2.TabIndex = 6;
+            this.Save2.Text = "Save 2";
+            this.Save2.UseVisualStyleBackColor = true;
+            this.Save2.Visible = false;
+            this.Save2.Click += new System.EventHandler(this.Save2_Click);
+            // 
+            // Save3
+            // 
+            this.Save3.Location = new System.Drawing.Point(52, 353);
+            this.Save3.Name = "Save3";
+            this.Save3.Size = new System.Drawing.Size(259, 67);
+            this.Save3.TabIndex = 7;
+            this.Save3.Text = "Save 3";
+            this.Save3.UseVisualStyleBackColor = true;
+            this.Save3.Visible = false;
+            this.Save3.Click += new System.EventHandler(this.Save3_Click);
+            // 
+            // Save4
+            // 
+            this.Save4.Location = new System.Drawing.Point(52, 426);
+            this.Save4.Name = "Save4";
+            this.Save4.Size = new System.Drawing.Size(259, 67);
+            this.Save4.TabIndex = 8;
+            this.Save4.Text = "Save 4";
+            this.Save4.UseVisualStyleBackColor = true;
+            this.Save4.Visible = false;
+            this.Save4.Click += new System.EventHandler(this.Save4_Click);
+            // 
+            // Saldo
+            // 
+            this.Saldo.AutoSize = true;
+            this.Saldo.BackColor = System.Drawing.Color.Transparent;
+            this.Saldo.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Saldo.Location = new System.Drawing.Point(1544, 0);
+            this.Saldo.Name = "Saldo";
+            this.Saldo.Size = new System.Drawing.Size(96, 37);
+            this.Saldo.TabIndex = 9;
+            this.Saldo.Text = "Saldo : ";
+            // 
+            // SparaKnapp
+            // 
+            this.SparaKnapp.Location = new System.Drawing.Point(341, 426);
+            this.SparaKnapp.Name = "SparaKnapp";
+            this.SparaKnapp.Size = new System.Drawing.Size(92, 26);
+            this.SparaKnapp.TabIndex = 10;
+            this.SparaKnapp.Text = "Spara";
+            this.SparaKnapp.UseVisualStyleBackColor = true;
+            this.SparaKnapp.Visible = false;
+            this.SparaKnapp.Click += new System.EventHandler(this.SparaKnapp_Click);
+            // 
+            // LaddaKnapp
+            // 
+            this.LaddaKnapp.Location = new System.Drawing.Point(341, 467);
+            this.LaddaKnapp.Name = "LaddaKnapp";
+            this.LaddaKnapp.Size = new System.Drawing.Size(92, 26);
+            this.LaddaKnapp.TabIndex = 11;
+            this.LaddaKnapp.Text = "Ladda";
+            this.LaddaKnapp.UseVisualStyleBackColor = true;
+            this.LaddaKnapp.Visible = false;
+            this.LaddaKnapp.Click += new System.EventHandler(this.LaddaKnapp_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.blackjackk;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1732, 737);
+            this.Controls.Add(this.LaddaKnapp);
+            this.Controls.Add(this.SparaKnapp);
+            this.Controls.Add(this.Saldo);
+            this.Controls.Add(this.Save4);
+            this.Controls.Add(this.Save3);
+            this.Controls.Add(this.Save2);
+            this.Controls.Add(this.Save1);
+            this.Controls.Add(this.sparListaBakgrund);
+            this.Controls.Add(this.Sparfiler);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.ReglerText);
+            this.Controls.Add(this.ReglerKnapp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            ((System.ComponentModel.ISupportInitialize)(this.sparListaBakgrund)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ReglerKnapp;
+        private System.Windows.Forms.Label ReglerText;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Sparfiler;
+        private System.Windows.Forms.PictureBox sparListaBakgrund;
+        private System.Windows.Forms.Button Save1;
+        private System.Windows.Forms.Button Save2;
+        private System.Windows.Forms.Button Save3;
+        private System.Windows.Forms.Button Save4;
+        private System.Windows.Forms.Label Saldo;
+        private System.Windows.Forms.Button SparaKnapp;
+        private System.Windows.Forms.Button LaddaKnapp;
+    }
+}
+
---
+++ Black Jack Projekt - WinForms/Black Jack.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int Balans = 1000;
+        int SparPosition = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            
+            this.Size = new System.Drawing.Size(1920, 1080);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            Saldo.Text = $"Saldo : {Balans}";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ReglerText.Visible == false)
+            {
+                ReglerText.Visible = true;
+                ReglerKnapp.BackColor = Color.Thistle;
+            }
+            else
+            ReglerText.Visible = false;
+            ReglerKnapp.BackColor = Color.White;
+
+        }
+
+        private void Sparfiler_Click(object sender, EventArgs e)
+        {
+            if (sparListaBakgrund.Visible == false)
+            {
+                VisaSparListaBakgrund();
+            }
+            else if (sparListaBakgrund.Visible == true)
+            {
+                NoSparListaBakgrund();
+            }
+            
+            
+        }
+        private void VisaSparListaBakgrund()
+        {
+            sparListaBakgrund.Visible = true;
+            Save1.Visible = true;
+            Save1.BringToFront();
+            Save2.Visible = true;
+            Save2.BringToFront();
+            Save3.Visible = true;
+            Save3.BringToFront();
+            Save4.Visible = true;
+            Save4.BringToFront();
+
+        }
+
+        private void NoSparListaBakgrund()
+        {
+            sparListaBakgrund.Visible = false;
+            Save1.Visible = false;
+            Save2.Visible = false;
+            Save3.Visible = false;
+            Save4.Visible = false;
+            SparaKnapp.Visible = false;
+            LaddaKnapp.Visible = false;
+
+        }
+
+        private void Save1_Click(object sender, EventArgs e)
+        {
+            SparaKnapp.Location = new Point(341, 209);
+            SparaKnapp.Visible = true;
+            LaddaKnapp.Location = new Point(341, 241);
+            LaddaKnapp.Visible = true;
+            SparPosition = 1;
+        }
+
+        private void Save2_Click(object sender, EventArgs e)
+        {
+            SparaKnapp.Location = new Point(341, 280);
+            SparaKnapp.Visible = true;
+            LaddaKnapp.Location = new Point(341, 321);
+            LaddaKnapp.Visible = true;
+            SparPosition = 2;
+        }
+
+        private void Save3_Click(object sender, EventArgs e)
+        {
+            SparaKnapp.Location = new Point(341, 353);
+            SparaKnapp.Visible = true;
+            LaddaKnapp.Location = new Point(341, 394);
+            LaddaKnapp.Visible = true;
+            SparPosition = 3;
+        }
+
+        private void Save4_Click(object sender, EventArgs e)
+        {
+            SparaKnapp.Location = new Point(341, 426);
+            SparaKnapp.Visible = true;
+            LaddaKnapp.Location = new Point(341, 467);
+            LaddaKnapp.Visible = true;
+            SparPosition = 4;
+        }
+
+        private void SparaKnapp_Click(object sender, EventArgs e)
+        {
+            SparaKnapp.Visible = false;
+            LaddaKnapp.Visible = false;
+            if (SparPosition == 1)
+            {
+                Properties.Settings.Default.Save1 = Balans;
+                Properties.Settings.Default.Save();
+            }
+            else if (SparPosition == 2)
+            {
+                Properties.Settings.Default.Save2 = Balans;
+                Properties.Settings.Default.Save();
+            }
+            else if (SparPosition == 3)
+            {
+                Properties.Settings.Default.Save3 = Balans;
+                Properties.Settings.Default.Save();
+            }
+            else if (SparPosition == 4)
+            {
+                Properties.Settings.Default.Save2 = Balans;
+                Properties.Settings.Default.Save();
+            }
+
+        }
+
+        private void LaddaKnapp_Click(object sender, EventArgs e)
+        {
+            SparaKnapp.Visible = false;
+            LaddaKnapp.Visible = false;
+            if (SparPosition == 1)
+            {
+                Balans = Properties.Settings.Default.Save1;
+                Saldo.Text = $"Saldo : {Balans}";
+            }
+            else if (SparPosition == 2)
+            {
+                Balans = Properties.Settings.Default.Save2;
+                Saldo.Text = $"Saldo : {Balans}";
+            }
+            else if (SparPosition == 3)
+            {
+                Balans = Properties.Settings.Default.Save3;
+                Saldo.Text = $"Saldo : {Balans}";
+            }
+            else if (SparPosition == 4)
+            {
+                Balans = Properties.Settings.Default.Save4;
+                Saldo.Text = $"Saldo : {Balans}";
+            }
+        }
+    }
+}
